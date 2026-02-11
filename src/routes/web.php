77f5a81cffaced61f21a09d5b5118fece38a55df
Nicu <nicu.plesa@net4media.net>
2026-02-11 13:50:46 +0200

    fix routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,14 +3,8 @@ use Illuminate\Support\Facades\Route;
 use nplesa\Observer\Http\Livewire\RequestsTable;
 use nplesa\Observer\Http\Livewire\DbTable;
 use nplesa\Observer\Http\Livewire\EventsTable;
-use nplesa\Observer\Http\Livewire\RecordedRequestsTable;
 use nplesa\Observer\Http\Livewire\JobsTable;
 
 Route::prefix('observer')->group(function () {
-    Route::get('/', fn() => view('observer::dashboard'));
-    Route::get('/requests', RequestsTable::class);
-    Route::get('/db', DbTable::class);
-    Route::get('/events', EventsTable::class);
-    Route::get('/recorded', RecordedRequestsTable::class);
-    Route::get('/jobs', JobsTable::class);
+
 });

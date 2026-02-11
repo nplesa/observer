5b8f991dec477598f872e6ca7c5091dbea3f195b
Nicolae Plesa <nicu.plesa@gmail.com>
2026-02-11 14:20:29 +0200

    Refactor ObserverServiceProvider formatting

--- src/ObserverServiceProvider.php
+++ src/ObserverServiceProvider.php
@@ -1,16 +1,13 @@
-
 <?php
 namespace nplesa\Observer;
 use Illuminate\Support\ServiceProvider;
 
 class ObserverServiceProvider extends ServiceProvider {
-    
     public function boot() {
         $this->publishes([__DIR__.'/config/observer.php'=>config_path('observer.php')],'config');
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
     }
-
     public function register(){
 
     }

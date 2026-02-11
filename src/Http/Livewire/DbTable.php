505a824b706a4da90fb0f0ee49b9fccebb8d04e8
Nicu <nicu.plesa@net4media.net>
2026-02-11 14:04:15 +0200

    .

--- src/Http/Livewire/DbTable.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace nplesa\Observer\Http\Livewire;
-use Livewire\Component;
-class DbTable extends Component { public function render() { return view('observer::livewire.db-table'); } }
--- src/config/observer.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'enable_requests_log'   => true,
-    'enable_db_log'         => false,
-    'enable_events_log'     => false,
-    'enable_jobs_log'       => false
-];

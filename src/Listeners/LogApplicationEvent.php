9d7fee7edc112b957cdc864e372dd241ff485da8
Nicu <nicu.plesa@net4media.net>
2026-02-16 12:22:12 +0200

    wip

--- src/Listeners/LogApplicationEvent.php
+++ src/Listeners/LogApplicationEvent.php
@@ -9,8 +9,9 @@ class LogApplicationEvent
         try {
             $eventClass = is_object($event) ? get_class($event) : (string)$event;
             $payloadArray = is_array($payload) ? $payload : [$payload];
-
-            \Log::info("Event fired: {$eventClass}", $payloadArray);
+            if(!is_null($payloadArray)) {
+                \Log::info("Event fired: {$eventClass}", $payloadArray);
+            }
         } catch (\Throwable $e) {
             \Log::warning("LogApplicationEvent: failed to log event: ".$e->getMessage());
         }
